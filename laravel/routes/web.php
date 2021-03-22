7509423f96fe7d572d33e28e4d8637e44986a895
marcopolello <marco-pol@hotmail.it>
2021-03-22 16:18:29 +0100

    landing page mountain

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -28,3 +28,4 @@ Route::view('/product', 'product');
 Route::view('/cart', 'cart');
 Route::view('/checkout', 'checkout');
 Route::view('/thankyou', 'thankyou');
+Route::view('/mountain', 'mountain');
